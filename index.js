86b9b71d090d11252d14ccfb86f8065f04be8e75
Ariful2634 <80531678+Ariful2634@users.noreply.github.com>
2023-08-20 15:55:15 +0600

    go back done

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function updatePrice(price) {
 function card1() {
     let price = getInput('spoon-tk');
     updatePrice(price)
-    setInput('t-price', totalPrice);
+    setInput('t-price', totalPrice.toFixed(2));
 
 }
 
@@ -24,38 +24,57 @@ function card2() {
     let pri = getInput('bowl-tk');
     updatePrice(pri)
 
-    setInput('t-price', totalPrice);
+    setInput('t-price', totalPrice.toFixed(2));
 }
 
 function card3() {
     let homePri = getInput('home-tk');
     updatePrice(homePri)
 
-    setInput('t-price', totalPrice);
+    setInput('t-price', totalPrice.toFixed(2));
 }
 
 function card4() {
     let cap = getInput('cap');
     updatePrice(cap)
 
-    setInput('t-price', totalPrice);
+    setInput('t-price', totalPrice.toFixed(2));
 }
 
 function card5() {
     let jersey = getInput('jersey');
     updatePrice(jersey)
 
-    setInput('t-price', totalPrice);
+    setInput('t-price', totalPrice.toFixed(2));
 }
 
 function card6() {
     let cates = getInput('cates');
     updatePrice(cates)
 
-    setInput('t-price', totalPrice);
+    setInput('t-price', totalPrice.toFixed(2));
 }
 
 
+// discount
+
+document.getElementById('co-btn').addEventListener('click',function(){
+ 
+    const inp = document.getElementById('inp');
+    const input = inp.value;
+    if(input=='SELL200'){
+        parcent=totalPrice-(totalPrice*0.20);
+        setInput('total',parcent.toFixed(2))
+        // console.log(typeof discTotal)
+        const discount = totalPrice-parcent;
+        setInput('disc',discount.toFixed(2))
+    }
+})
+
+
+function back(){
+    location.reload()
+}
 
 
 
